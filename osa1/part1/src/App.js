196c4858cfddbb7c51c6d3bb70832c8d5d902f4e
MinaSofi <94905864+MinaSofi@users.noreply.github.com>
2022-11-22 12:23:35 +0000

    Tehtävä 1.2 tehty!

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -14,19 +14,23 @@ const App = () => {
       </div>
     )
   }
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part} {props.exercises}
+        </p>
+      </div>
+    )
+  }
   
   const Content = (props) => {
     return (
       <div>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
       </div>
     )
   }
@@ -42,9 +46,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2}
-      part3={part3} exercises3={exercises3} />
+      <Content />
       <Total exercises1={exercises1} exercises2={exercises2}
       exercises3={exercises3} />
     </div>
